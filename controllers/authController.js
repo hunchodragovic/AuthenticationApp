a91561b1af68fc83366a0930d261bee24c9301a5
Ilyes.dragovic <mingo.huncho@gmail.com>
2025-04-01 10:40:00 +0100

    jwt

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  const foundUser = User.findOne({ email }).exec();
+  if (foundUser) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ name, email, password: hashedPassword });
+};
